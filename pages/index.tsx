54034a95408f08d4d661a6c02b7c585506de1153
Michael Cadiz <mike@cadizm.com>
2025-07-13 17:40:22 -0700

    Remove dev link

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,7 +56,6 @@ const Home: NextPage = () => {
       <main className="mb-auto pl-10 text-left">
         <ul className="tracking-widest">
           <li><a href="https://blog.cadizm.com">Blog</a></li>
-          <li><a href="https://dev.cadizm.com">Dev</a></li>
         </ul>
       </main>
 
